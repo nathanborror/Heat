b60bde9b6c14beaa2984ff77bbb50705fbae16c6
Nathan Borror <nathan@nathanborror.com>
2024-04-20 09:38:38 -0700

    improve suggestions prompt

--- HeatKit/Sources/HeatKit/Tools/GenerateSuggestions.swift
+++ HeatKit/Sources/HeatKit/Tools/GenerateSuggestions.swift
@@ -9,13 +9,13 @@ extension Tool {
             function: .init(
                 name: "suggested_prompts",
                 description: """
-                    Return a list of suggested prompts related to the conversation. These should be prompts a user \
-                    might say next in the conversation.
+                    Return a list of suggested replies related to the conversation. Remember, these are messages the
+                    user can respond with. Think about what they might want to say.
                     """,
                 parameters: .init(
                     type: .object,
                     properties: [
-                        "prompts": .init(type: .array, description: "A list of short prompts", items: .init(type: .string, minItems: 2, maxItems: 3)),
+                        "prompts": .init(type: .array, description: "A list of short replies", items: .init(type: .string, minItems: 2, maxItems: 3)),
                     ],
                     required: ["prompts"]
                 )
