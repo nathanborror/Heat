de32fca14453445758f033f045626d1124912311
Nathan Borror <nathan@nathanborror.com>
2024-04-29 22:23:41 -0700

    reset tools

--- HeatKit/Sources/HeatKit/Store.swift
+++ HeatKit/Sources/HeatKit/Store.swift
@@ -352,6 +352,7 @@ public final class Store {
         conversations = []
         preferences = .init()
         try resetAgents()
+        resetTools()
         logger.info("Persistence: all data reset")
     }
     
