e52cce3713933977341b8c0311a9db12a6f51fa4
Nathan Borror <nathan@nathanborror.com>
2024-09-20 20:49:41 -0700

    not used

--- HeatKit/Sources/HeatKit/Defaults.swift
+++ HeatKit/Sources/HeatKit/Defaults.swift
@@ -103,32 +103,4 @@ public struct Defaults {
             id: .perplexity,
             name: "Perplexity"
         )
-    
-    // MARK: - Models
-    
-    public static let anthropic_claude_3_5_sonnet = Model(id: "claude-3-5-sonnet-20240620", owner: "anthropic", contextWindow: 200_000, maxOutput: 8192)
-    public static let anthropic_claude_3_haiku = Model(id: "claude-3-haiku-20240307", owner: "anthropic", contextWindow: 200_000, maxOutput: 4096)
-    
-    public static let elevenlabs_monolingual_1 = Model(id: "eleven_monolingual_v1", owner: "elevenlabs")
-    
-    public static let fal_sdxl_fast = Model(id: "fast-sdxl", owner: "fal")
-    
-    public static let google_gemini_pro = Model(id: "gemini-pro", owner: "google")
-    
-    public static let groq_llama_3_1_70b_versatile = Model(id: "llama-3.1-70b-versatile", owner: "groq")
-    public static let groq_llama_3_1_8b_instant = Model(id: "llama-3.1-8b-instant", owner: "groq")
-    
-    public static let mistral_large = Model(id: "mistral-large-latest", owner: "mistral")
-    public static let mistral_small = Model(id: "mistral-small-latest", owner: "mistral")
-    public static let mistral_embed = Model(id: "mistral-embed", owner: "mistral")
-    
-    public static let openAI_gpt_4o = Model(id: "gpt-4o", owner: "openai")
-    public static let openAI_gpt_4o_mini = Model(id: "gpt-4o-mini", owner: "openai")
-    public static let openAI_dalle_3 = Model(id: "dall-e-3", owner: "openai")
-    public static let openAI_embedding_ada_2 = Model(id: "text-embedding-ada-002", owner: "openai")
-    public static let openAI_whisper_1 = Model(id: "whisper-1", owner: "openai")
-    public static let openAI_tts_1_hd = Model(id: "tts-1-hd", owner: "openai")
-    
-    public static let perplexity_llama_3_1_large = Model(id: "llama-3.1-sonar-large-128k-chat", owner: "perplexity")
-    public static let perplexity_llama_3_1_small = Model(id: "llama-3.1-sonar-small-128k-chat", owner: "perplexity")
 }
